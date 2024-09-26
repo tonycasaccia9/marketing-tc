19096d6b45039ee0a70a113937987a99c61a9899
tonycasaccia9 <tonycasacciagtm@gmail.com>
2024-09-26 10:46:52 -0700

    Added tabbed component

--- script.js
+++ script.js
@@ -53,7 +53,6 @@ navLinks.addEventListener("mouseout", hoverHandler.bind(1));
 // Underline effect
 const underline = document.querySelector(".underline");
 window.addEventListener("load", function (e) {
-  console.log(underline);
   underline.classList.add("underline--active");
 });
 
@@ -78,63 +77,93 @@ const dotsContainer = document.querySelector(".dots");
 // slider.style.scale = "0.5";
 // slider.style.transform = "translateX(-700px)";
 
-let curSlide = 0;
-const maxSlide = slides.length;
+const sliderCode = function () {
+  let curSlide = 0;
+  const maxSlide = slides.length;
 
-const createDots = function (slide) {
-  // select container and populate with dot html
-  slides.forEach(function (_, i) {
-    dotsContainer.insertAdjacentHTML(
-      "beforeend",
-      `<button class="dots__dot dots__dot--active" data-slide="${i}"></button>`
-    );
-  });
-};
-createDots(curSlide);
+  const createDots = function (slide) {
+    // select container and populate with dot html
+    slides.forEach(function (_, i) {
+      dotsContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot dots__dot--active" data-slide="${i}"></button>`
+      );
+    });
+  };
 
-const activateDots = function (slide) {
-  // Reset dots to non active
-  document.querySelectorAll(".dots__dot").forEach(function (dot) {
-    dot.classList.remove("dots__dot--active");
-  });
+  const activateDots = function (slide) {
+    // Reset dots to non active
+    document.querySelectorAll(".dots__dot").forEach(function (dot) {
+      dot.classList.remove("dots__dot--active");
+    });
 
-  // assign active dot
-  document
-    .querySelector(`.dots__dot[data-slide="${slide}"]`)
-    .classList.add("dots__dot--active");
-};
-activateDots(curSlide);
-const goToSlide = function (slide) {
-  slides.forEach(function (s, i) {
-    s.style.transform = `translateX(${(i - slide) * 100}%)`;
-  });
-};
-goToSlide(0);
+    // assign active dot
+    document
+      .querySelector(`.dots__dot[data-slide="${slide}"]`)
+      .classList.add("dots__dot--active");
+  };
+  const goToSlide = function (slide) {
+    slides.forEach(function (s, i) {
+      s.style.transform = `translateX(${(i - slide) * 100}%)`;
+    });
+  };
 
-const nextSlide = function () {
-  if (curSlide === maxSlide - 1) curSlide = 0;
-  else curSlide++;
-  goToSlide(curSlide);
-  activateDots(curSlide);
-};
-
-const prevSlide = function () {
-  if (curSlide === 0) curSlide = maxSlide - 1;
-  else curSlide--;
-  goToSlide(curSlide);
-  activateDots(curSlide);
-};
-
-btnRight.addEventListener("click", nextSlide);
-
-btnLeft.addEventListener("click", prevSlide);
-
-dotsContainer.addEventListener("click", function (e) {
-  if (e.target.classList.contains("dots__dot")) {
-    console.log(e.target);
-    curSlide = +e.target.dataset.slide;
-    console.log(curSlide);
+  const nextSlide = function () {
+    if (curSlide === maxSlide - 1) curSlide = 0;
+    else curSlide++;
     goToSlide(curSlide);
     activateDots(curSlide);
-  }
+  };
+
+  const prevSlide = function () {
+    if (curSlide === 0) curSlide = maxSlide - 1;
+    else curSlide--;
+    goToSlide(curSlide);
+    activateDots(curSlide);
+  };
+
+  btnRight.addEventListener("click", nextSlide);
+
+  btnLeft.addEventListener("click", prevSlide);
+
+  dotsContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      curSlide = +e.target.dataset.slide;
+      goToSlide(curSlide);
+      activateDots(curSlide);
+    }
+  });
+
+  const initSlider = function (slide) {
+    createDots(curSlide);
+    goToSlide(0);
+    activateDots(curSlide);
+  };
+  initSlider(curSlide);
+};
+sliderCode();
+
+/////////////////////////////////////////
+// Tabbed Section
+const tabs = document.querySelector(".tabs");
+const tabsBtnsContainer = document.querySelector(".tabs__btns");
+const tabsBtns = document.querySelectorAll(".tabs__btn");
+const tabsContent = document.querySelectorAll(".tabs__content");
+
+// Tab btn active
+tabsBtnsContainer.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains("tabs__btn")) return;
+  tabsBtns.forEach(function (btn) {
+    btn.classList.remove("tabs__btn--active");
+    e.target.classList.add("tabs__btn--active");
+  });
+
+  // Switch active tab content
+  tabsContent.forEach(function (tab) {
+    tab.classList.remove("tabs__content--active");
+  });
+  document
+    .querySelector(`.tabs__content--${e.target.dataset.tab}`)
+    .classList.add("tabs__content--active");
 });
