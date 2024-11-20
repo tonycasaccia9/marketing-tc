0fb4a23d2c41b41d027166d5229a7d164da4d340
tonycasaccia9 <tonycasacciagtm@gmail.com>
2024-11-20 11:13:46 -0800

    completely reworked styling

--- views/eyebrowsView.js
+++ views/eyebrowsView.js
@@ -1,30 +1,30 @@
-class EyebrowsView {
-  eyebrows = document.querySelectorAll(".eyebrow");
+// class EyebrowsView {
+//   eyebrows = document.querySelectorAll(".eyebrow");
 
-  constructor() {
-    this.addHandlerChangeEyebrow();
-    window.addEventListener("resize", this.checkViewportWidth.bind(this));
-    window.addEventListener("load", this.checkViewportWidth.bind(this));
-  }
+//   constructor() {
+//     this.addHandlerChangeEyebrow();
+//     window.addEventListener("resize", this.checkViewportWidth.bind(this));
+//     window.addEventListener("load", this.checkViewportWidth.bind(this));
+//   }
 
-  checkViewportWidth() {
-    if (window.innerWidth <= 450) {
-      this.eyebrows.forEach((eyebrow) => {
-        eyebrow.style.fontSize = "1.6rem";
-        eyebrow.style.fontWeight = "600";
-        const heading = eyebrow.textContent.toUpperCase();
-        eyebrow.textContent = heading;
-      });
-    } else {
-      this.eyebrows.forEach((eyebrow) => {
-        eyebrow.style.fontSize = "2.4rem";
-        eyebrow.style.fontWeight = "600";
-        const heading = eyebrow.textContent.toUpperCase();
-        eyebrow.textContent = heading;
-      });
-    }
-  }
-  addHandlerChangeEyebrow() {}
-}
+//   checkViewportWidth() {
+//     if (window.innerWidth <= 450) {
+//       this.eyebrows.forEach((eyebrow) => {
+//         eyebrow.style.fontSize = "1.6rem";
+//         eyebrow.style.fontWeight = "600";
+//         const heading = eyebrow.textContent.toUpperCase();
+//         eyebrow.textContent = heading;
+//       });
+//     } else {
+//       this.eyebrows.forEach((eyebrow) => {
+//         eyebrow.style.fontSize = "2.4rem";
+//         eyebrow.style.fontWeight = "600";
+//         const heading = eyebrow.textContent.toUpperCase();
+//         eyebrow.textContent = heading;
+//       });
+//     }
+//   }
+//   addHandlerChangeEyebrow() {}
+// }
 
-export default new EyebrowsView();
+// export default new EyebrowsView();
